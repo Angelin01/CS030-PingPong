fc7c541e1afbdaca05512ee0f84caa83735595dd
Angelin01 <angeloborsoiross@hotmail.com>
2017-08-25 15:05:57 -0300

    Tentativa de fixes 3 - talvez compile

--- P02-Gestao-Tarefas/pingpong.c
+++ P02-Gestao-Tarefas/pingpong.c
@@ -18,19 +18,19 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
     getcontext(task->mainContext);
 
     // Alocando stack
-    task->stack = malloc(sizeof(char) * STACKSIZE);
-    if(stack) {
-        task->taskContext.uc_stack.ss_sp = task->stack;
-        task->taskContext.uc_stack.ss_size = STACKSIZE;
-        task->taskContext.uc_stack.ss_flags = 0;
-        task->taskContext.uc_link = 0;
+    task->stack = malloc(sizeof(char) * SIZESTACK);
+    if(task->stack) {
+        task->tContext.uc_stack.ss_sp = task->stack;
+        task->tContext.uc_stack.ss_size = STACKSIZE;
+        task->tContext.uc_stack.ss_flags = 0;
+        task->tContext.uc_link = 0;
     }
     else {
         perror("Erro em task_create na criacao da stack: ");
         return (ERRSTACK);
     }
 
-    makecontext(task->tContext, start_func, 1, arg);
+    makecontext(task->tContext, (void*) start_func, 1, arg);
 
     #ifdef DEBUG
     printf("task_create: criou tarefa %d", task->id);
