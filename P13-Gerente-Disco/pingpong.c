072199cb84fc3ceba9f37da172d79e41dd2c3723
Angelo Borsoi Ross <suporte.estag2@cmc.pr.gov.br>
2017-11-27 17:10:49 -0200

    Pq diabos ! não ta funcionando? Ta mto bugado

--- P13-Gerente-Disco/pingpong.c
+++ P13-Gerente-Disco/pingpong.c
@@ -100,7 +100,6 @@ void pingpong_init() {
     /* Coisas do disco */
     /* --------------- */
 
-    diskdriver_init(&disk.numBlocks, &disk.blockSize);
     task_create(&diskManager, diskManager_body, "Toothless is love, Toothless is life");
     diskManager.userTask = 0;
 
@@ -746,15 +745,16 @@ void diskManager_body(void* arg) {
 }
 
 int diskdriver_init(int* numBlocks, int* blockSize) {
+    printf("Debug 1\n");
     if(!numBlocks || !blockSize) {
         return(-1);
     }
 
     // Iniciliza disco
-    if(!disk_cmd(DISK_CMD_INIT, 0, 0)) {
+    if(disk_cmd(DISK_CMD_INIT, 0, 0) < 0) {
         return(-1);
     }
-
+    printf("Debug 2\n");
     // Consulta o disco em si
     if((disk.numBlocks = disk_cmd(DISK_CMD_DISKSIZE, 0, 0)) < 0) {
         return(-1);
@@ -762,12 +762,12 @@ int diskdriver_init(int* numBlocks, int* blockSize) {
     if((disk.blockSize = disk_cmd(DISK_CMD_BLOCKSIZE, 0, 0)) < 0) {
         return(-1);
     }
-
+    printf("Debug 3\n");
     // Semaforo(mutex) para controle do disco
-    if(!sem_create(&disk.s_disk, 1)) {
+    if(sem_create(&disk.s_disk, 1) != 0) {
         return(-1);
     }
-
+    printf("Debug 4\n");
     // Seta os valores
 	*numBlocks = disk.numBlocks;
 	*blockSize = disk.blockSize;
